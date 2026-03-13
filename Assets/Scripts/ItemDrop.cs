51ef326cd9ac54a76efc5b61f280e788063e657f
Wojciech Opara <wwojtek660@gmail.com>
2026-03-13 21:35:10 +0100

    Make enemies drop XP

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    public GameObject item;
+
+    private static bool _quitting = false;
+
+    private void OnApplicationQuit()
+    {
+        _quitting = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (!_quitting && gameObject.scene.isLoaded)
+            Instantiate(item, transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Xp.cs
+++ Assets/Scripts/Xp.cs
@@ -4,4 +4,9 @@ public class Xp : MonoBehaviour
 {
     public float value = 0.0f;
     public float maxValue = 100.0f;
+
+    public void Add(float amount)
+    {
+        value = Mathf.Clamp(value + amount, 0.0f, maxValue);
+    }
 }
