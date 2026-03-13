40379fa6dcc80db2fa27d3879a5f131e4f4e46c7
Wojciech Opara <wwojtek660@gmail.com>
2026-03-13 18:19:19 +0100

    Add a simple enemy which follows player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementSpeed = 7.0f;
+
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        var maxMovementDistance = movementSpeed * Time.fixedDeltaTime;
+        var movementVector = Vector3.ClampMagnitude(_player.transform.position - transform.position, maxMovementDistance);
+        transform.localPosition += movementVector;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
