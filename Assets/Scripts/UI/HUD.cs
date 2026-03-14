11779e24a52ed844b07c24f4a1faa2b1b3e7f97b
Wojciech Opara <wwojtek660@gmail.com>
2026-03-14 20:37:41 +0100

    Add player stats and display them in the HUD

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -7,9 +7,11 @@ public class HUD : MonoBehaviour
     public Slider healthSlider;
     public Slider xpSlider;
     public TextMeshProUGUI levelText;
+    public TextMeshProUGUI statsText;
 
     public Health playerHealth;
     public PlayerXp playerXp;
+    public PlayerStats playerStats;
 
     private void Update()
     {
@@ -18,5 +20,6 @@ public class HUD : MonoBehaviour
         xpSlider.value = playerXp.value;
         xpSlider.maxValue = playerXp.maxValue;
         levelText.text = $"Level {playerXp.Level}";
+        statsText.text = $"Player stats:\nMovement speed: {playerStats.movementSpeed}\nAttack: {playerStats.attack}\nDefense: {playerStats.defense}";
     }
 }
