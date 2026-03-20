278659dda51d9d29a26a9cb348b25a1a7c5394d0
Wojciech Opara <wwojtek660@gmail.com>
2026-03-20 14:21:15 +0100

    Add a pause menu

---
+++ Assets/Scripts/UI/InGameUi.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class InGameUi : MonoBehaviour
+    {
+        public InputActionReference pauseAction;
+
+        public Slider healthSlider;
+        public Slider xpSlider;
+        public TextMeshProUGUI levelText;
+        public TextMeshProUGUI statsText;
+        public GameObject pauseMenu;
+
+        public PlayerReferences player;
+
+        private void Update()
+        {
+            if (pauseAction.action.WasPressedThisFrame())
+            {
+                var active = pauseMenu.activeSelf;
+                pauseMenu.SetActive(!active);
+            }
+
+            healthSlider.value = player.health.value;
+            healthSlider.maxValue = player.health.maxValue;
+            xpSlider.value = player.xp.value;
+            xpSlider.maxValue = player.xp.maxValue;
+            levelText.text = $"Level {player.xp.Level}";
+            statsText.text = $"Player stats:\nMovement speed: {player.stats.movementSpeed}\nAttack: {player.stats.attack}\nDefense: {player.stats.defense}";
+        }
+    }
+}
