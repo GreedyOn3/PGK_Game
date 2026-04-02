5cc111880da08b82e2f30196aad13b696d5dbd4a
Wojciech Opara <wwojtek660@gmail.com>
2026-04-03 00:03:32 +0200

    Add damage popups

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,18 +1,33 @@
+using UI;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float value = 10.0f;
+    [SerializeField] private GameObject damagePopupPrefab;
+    [SerializeField] private Vector3 damagePopupSpawnOffset = new(0.0f, 0.5f, 0.0f);
 
     public float Value => value;
 
     public void Remove(float amount)
     {
         value -= amount;
+        SpawnDamagePopup(amount);
 
         if (value <= 0.0f)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void SpawnDamagePopup(float amount)
+    {
+        var popupPosition = transform.position + damagePopupSpawnOffset;
+        // Add a random offset to the spawn position.
+        popupPosition += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+        var worldSpaceCanvas = GameObject.FindGameObjectWithTag("World Space Canvas");
+        var damagePopup = Instantiate(damagePopupPrefab, worldSpaceCanvas.transform);
+        damagePopup.transform.position = popupPosition;
+        damagePopup.GetComponent<DamagePopup>().SetDamage(amount);
     }
 }
---
+++ Assets/Scripts/UI/DamagePopup.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class DamagePopup : MonoBehaviour
+    {
+        public float floatSpeed = 2.0f;
+        public float lifetime = 1.0f;
+        public float fadeSpeed = 3.0f;
+
+        private TextMeshProUGUI _text;
+        private float _lifetimeTimer;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (_lifetimeTimer > lifetime)
+            {
+                Destroy(gameObject);
+            }
+
+            _lifetimeTimer += Time.deltaTime;
+
+            transform.position += Vector3.up * (floatSpeed * Time.deltaTime);
+            var textColor = _text.color;
+            textColor.a -= fadeSpeed * Time.deltaTime;
+            _text.color = textColor;
+
+            if (_camera == null)
+            {
+                return;
+            }
+
+            transform.LookAt(transform.position + _camera.transform.rotation * Vector3.forward, Vector3.up);
+        }
+
+        public void SetDamage(float damage)
+        {
+            _text.text = $"{damage}";
+        }
+    }
+}
