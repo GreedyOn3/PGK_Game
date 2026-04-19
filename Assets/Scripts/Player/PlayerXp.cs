19641dc8eb5a31e4919b3d2fbd0910096bed6cdd
N-Kac <kacper_nowak2019@poczta.fm>
2026-04-20 00:20:26 +0200

    Added basic XP scaling

--- Assets/Scripts/Player/PlayerXp.cs
+++ Assets/Scripts/Player/PlayerXp.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerXp : MonoBehaviour
 {
     [SerializeField] private int value = 0;
     [SerializeField] private int maxValue = 100;
+    [SerializeField] private List<SpecialLevel> specialLevels = new();
+
+    private SpecialLevel _currSpecial;
 
     public int Value => value;
     public int MaxValue => maxValue;
@@ -18,9 +23,44 @@ public class PlayerXp : MonoBehaviour
 
         while (value >= maxValue)
         {
-            Level++;
             value -= maxValue;
-            OnLevelUp?.Invoke();
+            LevelUp();
         }
+    }
+
+    private void LevelUp()
+    {
+        Level++;
+
+        if (_currSpecial == null)
+        {
+            SpecialLevel special = specialLevels.Find(x => x.Level == Level);
+            if (special != null)
+            {
+                _currSpecial = special;
+                maxValue += special.Amount;
+            }
+        } 
+        else
+        {
+            maxValue -= _currSpecial.Amount;
+            _currSpecial = null;
+        }
+
+        if (Level <= 20)
+            maxValue += 100;
+        else if (Level <= 40)
+            maxValue += 160;
+        else
+            maxValue += 220;
+
+        OnLevelUp?.Invoke();
+    }
+
+    [Serializable]
+    class SpecialLevel
+    {
+        public int Level;
+        public int Amount;
     }
 }
